62530c602a37ffb9510467489d644cfc0df93243
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-02-23 23:05:28 +0630

    ajax sorting part 2 asc and desc

--- resources/views/user/main/home.blade.php
+++ resources/views/user/main/home.blade.php
@@ -152,8 +152,8 @@
                             </div>
                         </div>
                     </div>
-
-                    @foreach ($pizzas as $pizza)
+                    <span id="dataList" class="row">
+                        @foreach ($pizzas as $pizza)
                         <div class="col-lg-4 col-md-6 col-sm-6 pb-1 " >
                             <div class="product-item bg-light mb-4" id="myForm">
                                 <div class="product-img position-relative overflow-hidden">
@@ -179,6 +179,8 @@
                             </div>
                         </div>
                     @endforeach
+                    </span>
+
                 </div>
             </div>
 
@@ -214,7 +216,37 @@
                     data : {'status' : 'asc'},
                     dataType: 'json', //essential
                     success: function(response) {
-                           console.log(response);
+                           $list = '';
+                           for($i=0;$i<response.length;$i++){
+                            $list += `
+                            <div class="col-lg-4 col-md-6 col-sm-6 pb-1 " >
+                            <div class="product-item bg-light mb-4" id="myForm">
+                                <div class="product-img position-relative overflow-hidden">
+                                    <img class="img-fluid w-100" style="height: 230px"
+                                        src="{{ asset('storage/${response[$i].image}') }}" alt="">
+                                    <div class="product-action">
+                                        <a class="btn btn-outline-dark btn-square" href=""><i
+                                                class="fa fa-shopping-cart"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href=""><i
+                                                class="fa-solid fa-circle-info"></i></a>
+                                        {{-- <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
+                                                <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
+                                                <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a> --}}
+                                    </div>
+                                </div>
+                                <div class="text-center py-4">
+                                    <a class="h6 text-decoration-none text-truncate" href="">${response[$i].name}</a>
+                                    <div class="d-flex align-items-center justify-content-center mt-2">
+                                        <h5>${response[$i].price}</h5>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            `;
+                           }
+                           $('#dataList').html($list);
+
                     }
                 })
             } else if ($eventOption == 'desc') {
@@ -224,7 +256,36 @@
                     data : {'status' : 'desc'},
                     dataType: 'json', //essential
                     success: function(response) {
-                        console.log(response);
+                        $list = '';
+                           for($i=0;$i<response.length;$i++){
+                            $list += `
+                            <div class="col-lg-4 col-md-6 col-sm-6 pb-1 " >
+                            <div class="product-item bg-light mb-4" id="myForm">
+                                <div class="product-img position-relative overflow-hidden">
+                                    <img class="img-fluid w-100" style="height: 230px"
+                                        src="{{ asset('storage/${response[$i].image}') }}" alt="">
+                                    <div class="product-action">
+                                        <a class="btn btn-outline-dark btn-square" href=""><i
+                                                class="fa fa-shopping-cart"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href=""><i
+                                                class="fa-solid fa-circle-info"></i></a>
+                                        {{-- <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
+                                                <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
+                                                <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a> --}}
+                                    </div>
+                                </div>
+                                <div class="text-center py-4">
+                                    <a class="h6 text-decoration-none text-truncate" href="">${response[$i].name}</a>
+                                    <div class="d-flex align-items-center justify-content-center mt-2">
+                                        <h5>${response[$i].price}</h5>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            `;
+                           }
+                           $('#dataList').html($list);
                     }
                 })
             }
