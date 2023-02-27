a3675e2e3cd3d8290d168fdb597eb32fd4885695
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-02-27 11:46:11 +0630

    add cart js and cartmanagement part 3

---
+++ public/js/cart.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    //when plus button click
+    $('.btn-plus').click(function(){
+        $parentNode = $(this).parents("tr");
+        $price = Number($parentNode.find('#pizzaPrice').text().replace("Kyats",""));
+        $qty = Number($parentNode.find('#qty').val());
+        $total = $price * $qty ;
+        $parentNode.find('#total').html($total + " Kyats");
+
+        summaryCalculation();
+
+
+
+    })
+
+    $('.btn-minus').click(function(){
+        //when minus button click
+        $parentNode = $(this).parents("tr");
+        $price = Number($parentNode.find('#pizzaPrice').text().replace("Kyats",""));
+        $qty = Number($parentNode.find('#qty').val());
+        $total = $price * $qty ;
+        $parentNode.find('#total').html($total + " Kyats");
+
+        summaryCalculation();
+    })
+
+    $('.btnRemove').click(function(){
+        //remove button click
+        $parentNode = $(this).parents("tr");
+        $parentNode.remove();
+
+        summaryCalculation();
+
+    })
+
+    function summaryCalculation(){
+        //calculate final total
+        $totalPrice = 0 ;
+        $('#dataTable tr').each(function(index,row){
+            $totalPrice += Number($(row).find('#total').text().replace("Kyats",""));
+
+        })
+
+        $('#subTotalPrice').html(`${$totalPrice} Kyats`);
+        $('#finalTotal').html(`${$totalPrice + 3000} Kyats`);
+    }
+})
--- resources/views/user/main/cart.blade.php
+++ resources/views/user/main/cart.blade.php
@@ -20,7 +20,7 @@
     <div class="container-fluid">
         <div class="row px-xl-5">
             <div class="col-lg-8 table-responsive mb-5">
-                <table class="table table-light table-borderless table-hover text-center mb-0">
+                <table class="table table-light table-borderless table-hover text-center mb-0" id="dataTable">
                     <thead class="thead-dark">
                         <tr>
                             <th>Products</th>
@@ -32,11 +32,11 @@
                     </thead>
                     <tbody class="align-middle">
                         @foreach ($cartList as $c)
-                            <tr>
+                            <tr >
                                 <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;">
                                     {{ $c->pizza_name }}</td>
-                                <td class="align-middle" >{{ $c->pizza_price }} Kyats</td>
-                                <input type="hidden" name="" value="{{$c->pizza_price}}" id="pizzaPrice">
+                                <td class="align-middle" id="pizzaPrice" >{{ $c->pizza_price }} Kyats</td>
+                                {{-- <input type="hidden" name="" value="{{$c->pizza_price}}" id="pizzaPrice"> --}}
                                 <td class="align-middle">
                                     <div class="input-group quantity mx-auto" style="width: 100px;">
                                         <div class="input-group-btn">
@@ -70,7 +70,7 @@
                     <div class="border-bottom pb-2">
                         <div class="d-flex justify-content-between mb-3">
                             <h6>Subtotal</h6>
-                            <h6>{{ $totalPrice }}</h6>
+                            <h6 id="subTotalPrice">{{ $totalPrice }}</h6>
                         </div>
                         <div class="d-flex justify-content-between">
                             <h6 class="font-weight-medium">Delivery</h6>
@@ -80,7 +80,7 @@
                     <div class="pt-2">
                         <div class="d-flex justify-content-between mt-2">
                             <h5>Total</h5>
-                            <h5>{{ $totalPrice + 3000 }}</h5>
+                            <h5 id="finalTotal">{{ $totalPrice + 3000 }}</h5>
                         </div>
                         <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</button>
                     </div>
@@ -92,31 +92,8 @@
 @endsection
 
 @section('scriptSource')
-    <script>
-        $(document).ready(function() {
-            $('.btn-plus').click(function(){
-                $parentNode = $(this).parents("tr");
-                $price = $parentNode.find('#pizzaPrice').val();
-                $qty = Number($parentNode.find('#qty').val())  ;
-                $total = $price * $qty ;
-                $parentNode.find('#total').html($total + " Kyats");
+    <script src="{{ asset('js/cart.js')}}">
 
-            })
-
-            $('.btn-minus').click(function(){
-                $parentNode = $(this).parents("tr");
-                $price = $parentNode.find('#pizzaPrice').val();
-                $qty = Number($parentNode.find('#qty').val()) ;
-
-                $total = $price * $qty ;
-                $parentNode.find('#total').html($total + " Kyats");
-            })
-
-            $('.btnRemove').click(function(){
-                $parentNode = $(this).parents("tr");
-                $parentNode.remove();
-            })
-        })
     </script>
 @endsection
 
